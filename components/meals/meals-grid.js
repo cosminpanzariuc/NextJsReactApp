07e95db82eafa84e84836e00ee21f8fdf18d8afa
Cosmin Panzariuc <cosminilie.panzariuc@cognizant.com>
2025-10-31 16:18:25 +0200

    fix

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -4,7 +4,7 @@ import MealItem from "@/components/meals/meal-item";
 export default function MealsGrid({meals}) {
     return <ul className={classes.meals}>
         {meals.map(meal => <li key={meal.id}>
-            <MealItem {...meals}/>
+            <MealItem {...meal}/>
         </li>)}
     </ul>
 }
