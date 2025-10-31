5fe9f87b1dec94dbf9616dbbad844a171e92648e
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2025-10-31 21:39:45 +0200

    fix typo

--- app/meals/page.js
+++ app/meals/page.js
@@ -9,7 +9,7 @@ export default async function MealsPage() {
     return <>
         <header className={classes.header}>
             <h1>Delicious meals, created <span className={classes.highlight}>by you</span></h1>
-            <p>Choose your favorite recipe and cook ot ypurself. It is easy and fun!</p>
+            <p>Choose your favorite recipe and cook ot yourself. It is easy and fun!</p>
             <p className={classes.cta}>
                 <Link href="/meals/share">
                     Share Your Favorite Recipe
